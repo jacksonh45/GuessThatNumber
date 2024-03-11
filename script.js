39883858dab150c0e81b82a2da03f32e345d6140
Jackson Harrison <Jackson.Harrison@stu.barren.kyschools.us>
2024-03-11 11:01:12 -0500

    Created starting template for game.

--- script.js
+++ script.js
@@ -13,3 +13,32 @@ let attempts;
 let guess;
 // For storing the user's response to play again or not play again
 let playAgain;
+
+// Starting alert message
+alert(`Welcome to the GUESS THAT NUMBER ! Please click OK to start the game`)
+
+// Game restarts as long as restartGame has a value of true
+while(restartGame){
+
+    // Asks the user to enter a number to set the upper bound for the random number that will be created. Also, using parseInt to attempt to convert the users response to a number value
+   rangeNum = prompt(`Please enter a maximum number for the range:`)
+   rangeNum = parseInt(rangeNum)
+
+   // Verifies the users entry for a range number is a number greater than one
+    while (rangeNum <= 1 || !rangeNum){
+       rangeNum = prompt(`Please enter a number greater than 1`)
+        rangeNum = parseInt(rangeNum)
+    }
+    randomNum = Math.floor(Math.random() * rangeNum) + 1
+
+    alert(randomNum)
+
+    attempts = parseInt( prompt(`Please enter a number of attempts allowed`))
+
+    while (attempts < 1 || !attempts || attempts >= rangeNum ){
+        attempts = prompt(`Please enter a number from 1 to ${rangeNum - 1} `)
+         attempts = parseInt(attempts)
+     }
+
+    break
+}
